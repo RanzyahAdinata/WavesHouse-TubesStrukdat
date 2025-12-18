e230c44dfc33e8bebcc250060e60310588fdf0bb
Yagami Codes <122334993+RanzyahAdinata@users.noreply.github.com>
2025-12-18 21:37:52 +0700

    update interface on mll.h

--- mll.h
+++ mll.h
@@ -1,6 +1,9 @@
 #ifndef MLL_H_INCLUDED
 #define MLL_H_INCLUDED
 
+#ifndef MLL_H
+#define MLL_H
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -85,5 +88,7 @@ int hitungStokBarangTertentu(list_gudang LG, string idGudang, string idBarang);
 
 void cariBarangRusak(list_gudang LG);
 
+/*FITUR : MENU*/
 void ui();
 #endif
+
