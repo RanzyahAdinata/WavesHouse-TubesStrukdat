33da6c43b741a6c1f7c2dbd71206bd9373a803bd
Yagami Codes <122334993+RanzyahAdinata@users.noreply.github.com>
2025-12-27 12:15:31 +0700

    declare func sorting on mll.h

--- mll.h
+++ mll.h
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-/* INFOTYPE gudang n barang*/
+/* INFOTYPE */
 
 struct infotype_barang {
     string id_barang;
@@ -20,7 +20,7 @@ struct infotype_gudang {
     string nama_gudang;
 };
 
-/* ELEMEN gudang n barang */
+/* ELEMEN */
 
 struct elm_barang {
     infotype_barang info;
@@ -30,15 +30,15 @@ struct elm_barang {
 struct elm_gudang {
     infotype_gudang info;
     elm_gudang *next;
-    elm_barang *firstChild;   // pointer ke list barang
+    elm_barang *firstChild;
 };
 
-/*  POINTER gudang n barang */
+/* POINTER */
 
 typedef elm_barang* adr_barang;
 typedef elm_gudang* adr_gudang;
 
-/* LIST gudang n barang */
+/* LIST */
 
 struct list_barang {
     adr_barang first;
@@ -48,45 +48,74 @@ struct list_gudang {
     adr_gudang first;
 };
 
-/* CRUD disini */
+/* CREATE */
 
 void createListGudang(list_gudang &LG);
 void createListBarang(list_barang &LB);
 
+/* ALOKASI */
+
 adr_gudang alokasiGudang(infotype_gudang x);
 adr_barang alokasiBarang(infotype_barang x);
+
+/* INSERT */
 
 void insertGudang(list_gudang &LG, adr_gudang G);
 void insertBarangKeGudang(list_gudang &LG, string idGudang, adr_barang B);
 
+/* DELETE */
+
 void deleteGudang(list_gudang &LG, string idGudang);
 void deleteBarang(list_gudang &LG, string idGudang, string idBarang);
+
+/* SEARCH & CEK */
 
 bool cekgudangnull(adr_gudang x);
 bool cekbarangnull(adr_barang x);
 adr_gudang searchGudang(list_gudang LG, string idGudang);
 adr_barang searchBarang(adr_gudang G, string idBarang);
 
+/* SHOW */
+
 void showSemuaGudang(list_gudang LG);
 void showBarangGudang(list_gudang LG, string idGudang);
 void showSemuaBarangUnik(list_gudang LG);
 void showbarangtertentu(adr_barang q);
 
-/* FITUR : hitung stok nya */
+/* HITUNG STOK */
 
 adr_barang stokTerbanyak(list_gudang LG);
-adr_barang stokTerbanyakPergudang(list_gudang LG, adr_gudang z);
 adr_barang stokTersedikit(list_gudang LG);
+adr_barang stokTerbanyakPergudang(list_gudang LG, adr_gudang z);
 adr_barang stokTersedikitPergudang(list_gudang LG, adr_gudang z);
+
 int hitungTotalStokGudang(list_gudang LG, string idGudang);
 int hitungStokBarangTertentu(list_gudang LG, string idGudang, string idBarang);
 
-/* FITUR : cari barang spesifik */
+/* CARI BARANG */
 
 void cariBarangRusak(list_gudang LG);
 
-/*FITUR : MENU*/
+/* SORTING GUDANG (ALFABET) */
+
+void sortGudangAsc(list_gudang &LG);
+void sortGudangDesc(list_gudang &LG);
+
+/* SORTING BARANG PER GUDANG (STOK) */
+
+void sortBarangStokAsc(adr_gudang G);   
+void sortBarangStokDesc(adr_gudang G);
+
+/* SORTING BARANG SEMUA GUDANG (STOK) */
+
+void sortBarangStokAscGlobal(list_gudang &LG);
+void sortBarangStokDescGlobal(list_gudang &LG);
+
+/* MENU */
+
 void ui();
+
 #endif
+
 
 
